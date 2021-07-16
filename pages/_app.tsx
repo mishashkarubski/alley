a13d4f447037a7082f080bf32c5397e11b3cb888
mishashkarubski <michaelshkarubski@gmail.com>
2021-07-16 21:59:38 +0300

    Come more configuration...

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import '../styles/globals.css';
+import {AppProps} from "next/dist/next-server/lib/router/router";
+import Head from "next/head";
+import React from "react";
+
+function MyApp({Component, pageProps}: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <title>Alley</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                {/*<link rel="preconnect" href="https://fonts.googleapis.com"/>*/}
+                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;700&display=swap" rel="stylesheet" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
+}
+
+export default MyApp;
